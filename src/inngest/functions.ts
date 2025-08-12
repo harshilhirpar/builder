2518e2881e84e3b875ecb33689cd45bc6584aea9
harshil202 <hirparah499@gmail.com>
2025-08-11 23:32:42 -0400

    added e2b sandbox and docker

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { openai, createAgent } from "@inngest/agent-kit";
+import {Sandbox} from "@e2b/code-interpreter";
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async() => {
+      const sandbox = await Sandbox.create("vib-harshil-3");
+      return sandbox.sandboxId;
+    })
     // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
       name: "summarizer",
@@ -17,6 +23,12 @@ export const helloWorld = inngest.createFunction(
     );
     console.log(output);
 
-    return {output};
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+
+    return {output, sandboxUrl};
   },
 );
